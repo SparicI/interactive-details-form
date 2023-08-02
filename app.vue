25468b5244ad0ebbea0ececf01f9cac53cfb0e52
Ivana Sparic <sparicivana.biz@gmail.com>
2023-08-02 19:09:27 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,136 @@
+<template>
+  <main>
+    <h1 class="visually-hidden">Credit card form</h1>
+    <section class="cards-section">
+      <div>
+        <img
+          src="./assets/images/bg-card-back.png"
+          alt="Card back"
+          class="card__back"
+        >
+
+        <p class="card__data card__data--cvc">{{ data?.cvc === '' ? '000' : data?.cvc }}</p>
+
+      </div>
+
+      <div>
+        <img
+          src="./assets/images/bg-card-front.png"
+          alt="Card Front"
+          class="card__front"
+        >
+        <div class="card__wrapper">
+          <img
+            src="./assets/images/card-logo.svg"
+            alt="Card logo"
+            class="card__logo"
+          >
+          <div>
+            <p class="card__data font-size-500">{{ data?.cardNumber === '' ? '0000 0000 0000 0000' : data?.cardNumber }}
+            </p>
+            <div class="flex-row margin-block-start-300 justify-content-space-between">
+              <p class="card__data text-transform-uppercase">{{ data?.cardholderName === '' ? 'Jane Appleseed'
+                :
+                data?.cardholderName }}</p>
+              <p class="card__data">{{ data?.month === '' ? '00' : data?.month }}/{{ data?.year === '' ? '00'
+                : data?.year
+              }}</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </section>
+    <section class="form-section">
+      <BaseForm />
+    </section>
+  </main>
+</template>
+
+<script setup>
+
+const data = useState('form')
+
+</script>
+
+<style scoped>
+.cards-section {
+  min-height: 235px;
+  position: relative;
+
+  background-image: url('./assets/images/bg-main-mobile.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.form-section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-block: 5.5rem 1rem;
+}
+
+.card__back,
+.card__front {
+  position: fixed;
+  width: 285px;
+  height: 160px;
+}
+
+.card__back {
+  top: 2rem;
+  right: 1rem;
+}
+
+.card__front {
+  top: 7.65rem;
+  left: 1rem;
+}
+
+.card__data {
+  color: var(--white);
+  font-size: var(--font-size-100);
+  letter-spacing: 1.5px;
+}
+
+.card__logo {
+  width: 50px;
+}
+
+.card__wrapper {
+  position: fixed;
+  top: 137px;
+  left: 33px;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+}
+
+.card__data--cvc {
+  position: fixed;
+  top: 103px;
+  right: 50px;
+}
+
+@media (min-width: 600px) {
+
+  main {
+    flex-direction: row;
+  }
+
+  .cards-section {
+    flex: 1;
+    background-image: url('./assets/images/bg-main-desktop.png');
+  }
+
+
+  .card__back,
+  .card__front {
+    width: 445px;
+    height: 245px;
+  }
+}
+</style>
---
+++ components/BaseForm.vue
@@ -0,0 +1,150 @@
+<template>
+    <form
+        @submit.prevent="submit"
+        class="form flex-column"
+    >
+        <div class="form__group flex-column">
+            <label
+                for="cardholderName"
+                class="form__label"
+            >
+                Cardholder name
+            </label>
+            <input
+                id="cardholderName"
+                class="form__input"
+                type="text"
+                name="cardholderName"
+                v-model="form.cardholderName"
+                placeholder=" e.g. Jane Appleseed"
+            />
+        </div>
+
+        <div class="form__group flex-column">
+            <label
+                for="cardNumber"
+                class="form__label"
+            >
+                Card number
+            </label>
+            <input
+                id="cardNumber"
+                class="form__input"
+                type="text"
+                name="cardNumber"
+                v-model="form.cardNumber"
+                placeholder="e.g. 1234 5678 9123 0000"
+            />
+        </div>
+
+        <div class="flex-row">
+            <div class="form__group flex-column width-50">
+                <label
+                    for="number"
+                    class="form__label"
+                >
+                    Exp. date (mm/yy)
+                </label>
+                <div class="flex-row">
+                    <input
+                        id="month"
+                        class="form__input"
+                        type="text"
+                        name="month"
+                        v-model="form.month"
+                        placeholder="MM"
+                    />
+                    <input
+                        id="year"
+                        class="form__input"
+                        type="text"
+                        name="year"
+                        v-model="form.year"
+                        placeholder="YY"
+                    />
+                </div>
+            </div>
+
+            <div class="form__group flex-column width-50">
+                <label
+                    for="cvc"
+                    class="form__label"
+                >
+                    Cvc
+                </label>
+                <input
+                    id="cvc"
+                    class="form__input"
+                    type="number"
+                    name="cvc"
+                    v-model="form.cvc"
+                    placeholder="e.g. 123"
+                />
+            </div>
+        </div>
+
+        <button
+            type="submit"
+            class="form__button"
+        >Confirm</button>
+    </form>
+</template>
+
+<script setup>
+
+const form = useState('form', () => ({
+    cardholderName: '',
+    cardNumber: '',
+    month: '',
+    year: '',
+    cvc: ''
+}))
+
+
+</script>
+<style scoped>
+.form {
+    max-width: 400px;
+    gap: var(--spacing-400);
+    padding-inline: var(--spacing-600);
+}
+
+.form__label {
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+}
+
+.form__input {
+    padding: var(--spacing-200);
+    border: 1.4px solid var(--light-grayish-violet);
+    border-radius: var(--border-radius-soft);
+    width: 100%;
+}
+
+.form__input::placeholder {
+    font-size: 18px;
+    color: var(--dark-grayish-violet);
+    font-weight: 700;
+    opacity: 0.37;
+}
+
+.form__button {
+    margin-block-start: var(--spacing-400);
+    padding: var(--spacing-300);
+    border-radius: var(--border-radius-soft);
+
+    background-color: var(--very-dark-violet);
+    color: var(--white);
+}
+
+@media (min-width: 600px) {
+    .form__label {
+        font-size: var(--font-size-300);
+    }
+
+    .form__input::placeholder {
+        font-size: 22px;
+    }
+}
+</style>
