923b196040b59526c95e5beb952df00508138e67
Ivana Sparic <sparicivana.biz@gmail.com>
2023-08-04 12:27:44 +0200

    Update gitignore

--- .nuxt/dist/server/styles.mjs
+++ .nuxt/dist/server/styles.mjs
@@ -1,6 +1,6 @@
 const interopDefault = r => r.default || r || []
 export default {
   "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.963be161.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.cf00f4cc.mjs').then(interopDefault),
-  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a001b7ed.mjs').then(interopDefault)
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a001b7ed.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.cf00f4cc.mjs').then(interopDefault)
 }
